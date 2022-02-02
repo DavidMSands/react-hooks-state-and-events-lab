807cf939cc98f7f149571ee53ff89a3ac386b954
David <Davidmaxsands@gmail.com>
2022-02-01 23:50:47 -0700

    complete" git push q dsfd"

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+
+  function clickHandler() {
+    setInCart(inCart => !inCart)
+  }
+
+  const liClass = inCart ? 'in-cart' : ''
+  const inCartText = inCart ? 'Remove From Cart' : 'Add to Cart'
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={clickHandler} className="add">{inCartText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+const [selectedCategory, setSelectedCategory] = useState('All')
+
+function changeHandler(e) {
+  setSelectedCategory(e.target.value)
+}
+
+const newFilter = items.filter(item => item.category === selectedCategory)
+
+const filter = selectedCategory === "All" ? items : newFilter
+
+console.log(selectedCategory)
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={changeHandler} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +24,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filter.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
